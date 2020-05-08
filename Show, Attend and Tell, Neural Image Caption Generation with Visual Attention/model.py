a72ac0997a70c1536c0543fc3fde21fed28051f3
uzi0espil <ousama.esbel@gmail.com>
2020-05-08 15:31:04 +0000

    Fix some errors in the model

--- Show, Attend and Tell, Neural Image Caption Generation with Visual Attention/model.py
+++ Show, Attend and Tell, Neural Image Caption Generation with Visual Attention/model.py
@@ -9,7 +9,7 @@ class CNNEncoder(tf.keras.Model):
         self.base_model = tf.keras.applications.InceptionV3(include_top=False, weights="imagenet")
         for layer in self.base_model.layers:
             layer.trainable = False
-        self.fc = tf.keras.layers.Dense(units, activation="relu")
+        self.fc = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units, activation="relu"))
         
     def call(self, X):
         X = self.base_model(X)
@@ -45,7 +45,7 @@ class LuongAttention(tf.keras.layers.Layer):
     def __init__(self, units):
         super(LuongAttention, self).__init__()
         self.dot = tf.keras.layers.Dot((2, 2))
-        self.fc = tf.keras.layers.Dense(units, activation='tanh')
+        self.fc = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units, activation='tanh'))
         
     def call(self, dec_outputs, enc_outputs):
         scores = self.dot([dec_outputs, enc_outputs])
@@ -77,21 +77,22 @@ class RNNDecoder(tf.keras.Model):
         
         self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-        self.gru = tf.keras.layers.GRU(units, return_state=True, return_sequences=True)
-        self.attention = BahdanauAttention(units)
+        self.lstm = tf.keras.layers.LSTM(units, return_state=True, return_sequences=True)
         
-        self.fc1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units, activation='relu'))
-        self.fc2 = tf.keras.layers.Dense(vocab_size, activation="softmax")
+        self.fc1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units))
+        self.fc2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(vocab_size, activation="softmax"))
         
     def call(self, x, hidden, enc_output):
         attention_weights = None
         x = self.embedding(x)
         
         if isinstance(self.attention, BahdanauAttention):
-            context_vector, attention_weights = self.attention(hidden, enc_output)
+            # we only care about hidden states when computing the attention
+            context_vector, attention_weights = self.attention(hidden[0], enc_output)
             x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
         
-        x, state = self.gru(x)
+        x, h_states, c_states = self.lstm(x, initial_state=hidden)
+        states = [h_states, c_states]
         
         if isinstance(self.attention, LuongAttention):
             x, attention_weights = self.attention(x, enc_output)
@@ -99,13 +100,26 @@ class RNNDecoder(tf.keras.Model):
         # shape == (batch_size, max_length, hidden_size)
         x = self.fc1(x)
         
-        # x shape == (batch_size * max_length, hidden_size)
-        x = tf.reshape(x, (-1, x.shape[2]))
-        
         # output shape == (batch_size * max_length, vocab)
         x = self.fc2(x)
         
-        return x, state, attention_weights
+        return x, states, attention_weights
         
     def reset_state(self, batch_size):
-        return tf.zeros((batch_size, self.units))
+        return [tf.zeros((batch_size, self.units)), tf.zeros((batch_size, self.units))]
+    
+
+class ImageCaption(tf.keras.Model):
+    
+    def __init__(self, target_vocab_size, vocab_size, embedding_dim, units):
+        self.encoder = Encoder(embedding_dim)
+        self.decoder = Decoder(target_vocab_size, vocab_size, embedding_dim, units, attention="multiplicative")
+        
+    def call(X, **kwargs):
+        enc_input, dec_input = X['enc_input'], X['dec_input']
+        hidden = self.decoder.reset_state(X.shape[0])
+        
+        enc_output = self.encoder(enc_input)
+        decoder_output, attention = self.decoder(dec_input, hidden, enc_input)
+        
+        return decoder_output, attention
