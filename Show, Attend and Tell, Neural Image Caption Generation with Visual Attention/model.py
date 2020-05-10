f0d9f508ccc357bfd7ec53106e646211bfb03d71
uzi0espil <ousama.esbel@gmail.com>
2020-05-10 17:36:51 +0200

    run-v2 is able to run the CaptionModel using keras fit function instead of custom loop training

--- Show, Attend and Tell, Neural Image Caption Generation with Visual Attention/model.py
+++ Show, Attend and Tell, Neural Image Caption Generation with Visual Attention/model.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 
 
-class CNNEncoder(tf.keras.Model):
+class CNNEncoder(tf.keras.layers.Layer):
     
-    def __init__(self, units):
+    def __init__(self, units, inception_shape):
         super(CNNEncoder, self).__init__()
         # load Inception model and freeze the upper layers
         self.base_model = tf.keras.applications.InceptionV3(include_top=False, weights="imagenet")
+        self.reshape = tf.keras.layers.Reshape(inception_shape)
         for layer in self.base_model.layers:
             layer.trainable = False
         self.fc = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units, activation="relu"))
@@ -14,7 +15,7 @@ class CNNEncoder(tf.keras.Model):
     def call(self, X):
         X = self.base_model(X)
         # merged the width and hight of feature map into a vector. so the reshape will convert (None, 8, 8, 2048) to (None, 64, 2048)
-        X = tf.reshape(X, (X.shape[0], -1, X.shape[3]))
+        X = self.reshape(X)
         return self.fc(X)
     
 
@@ -53,18 +54,17 @@ class LuongAttention(tf.keras.layers.Layer):
         attention_weights = tf.nn.softmax(scores)
         
         context_vector = tf.keras.layers.dot([attention_weights, enc_outputs], axes=(2,1))
-        
         y = tf.keras.layers.concatenate([context_vector, dec_outputs])
         y = self.fc(y)
         
         return y, attention_weights
     
 
-class RNNDecoder(tf.keras.Model):
+class RNNDecoder(tf.keras.layers.Layer):
     
     attentions = ["additive", "multiplicative"]
     
-    def __init__(self, vocab_size, embedding_dim, units, attention="additive"):
+    def __init__(self, vocab_size, embedding_dim, units, attention="additive", mask=True):
         super(RNNDecoder, self).__init__()
         if attention is not None and attention.lower() not in self.attentions:
             raise ValueError("attention value is not correct, should be either {} or None".format(attentions))
@@ -76,7 +76,7 @@ class RNNDecoder(tf.keras.Model):
             self.attention = None
         
         self.units = units
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim, mask_zero=mask)
         self.lstm = tf.keras.layers.LSTM(units, return_state=True, return_sequences=True)
         
         self.fc1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units))
@@ -91,7 +91,7 @@ class RNNDecoder(tf.keras.Model):
             context_vector, attention_weights = self.attention(hidden[0], enc_output)
             x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
         
-        x, h_states, c_states = self.lstm(x, initial_state=hidden)
+        x, h_states, c_states = self.lstm(x)
         states = [h_states, c_states]
         
         if isinstance(self.attention, LuongAttention):
@@ -107,19 +107,23 @@ class RNNDecoder(tf.keras.Model):
         
     def reset_state(self, batch_size):
         return [tf.zeros((batch_size, self.units)), tf.zeros((batch_size, self.units))]
-    
+
 
 class ImageCaption(tf.keras.Model):
+    """
+    Model that encapsulate Encoder and Decoder layers. Supports only Luong Attention.
+    """
     
-    def __init__(self, target_vocab_size, vocab_size, embedding_dim, units):
-        self.encoder = Encoder(embedding_dim)
-        self.decoder = Decoder(target_vocab_size, vocab_size, embedding_dim, units, attention="multiplicative")
+    def __init__(self, inception_shape, target_vocab_size, embedding_dim, units):
+        super(ImageCaption, self).__init__()
+        self.encoder = CNNEncoder(units, inception_shape)
+        self.decoder = RNNDecoder(target_vocab_size, embedding_dim, units, attention="multiplicative")
         
-    def call(X, **kwargs):
-        enc_input, dec_input = X['enc_input'], X['dec_input']
-        hidden = self.decoder.reset_state(X.shape[0])
+    def call(self, X, **kwargs):
+        enc_input, dec_input = X
         
         enc_output = self.encoder(enc_input)
-        decoder_output, attention = self.decoder(dec_input, hidden, enc_input)
         
-        return decoder_output, attention
+        decoder_output, _, attention_weights = self.decoder(dec_input, None, enc_output)
+        
+        return decoder_output, attention_weights
